2e43db6dd243df19e63a87f70041a44d30f55013
Jenkin$ <jenkins.wav@gmail.com>
2024-10-20 16:12:40 +0200

    ex00 + main ok

--- CPP05/ex00/srcs/main.cpp
+++ CPP05/ex00/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lpr <lpr@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/20 13:24:16 by lpr               #+#    #+#             */
-/*   Updated: 2024/10/20 15:53:06 by lpr              ###   ########.fr       */
+/*   Updated: 2024/10/20 16:11:16 by lpr              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,26 @@
 
 int main()
 {
-    Bureaucrat b("phillip", 155);
-    std::cout << b << std::endl;
-    b.upGrade();
-    std::cout << b << std::endl;
+    Bureaucrat billy("billy", 149);
+    Bureaucrat tom("tom", 2);
+    Bureaucrat lola("lola", 245);
+    std::cout << std::endl;
+    std::cout << billy << std::endl;
+    std::cout << tom << std::endl;
+    std::cout << lola << std::endl;
+    
+    std::cout << "\n ------billy------ " << std::endl;
+    billy.downGrade();
+    std::cout << billy << std::endl;
+    billy.downGrade();
+    std::cout << billy << std::endl;
+
+    std::cout << "\n ------tom------ " << std::endl;
+    tom.upGrade();
+    std::cout << tom << std::endl;
+    tom.upGrade();
+    std::cout << tom << std::endl;
+    std::cout << std::endl;
+    
     return 0;
 }
