e18f0040e4afea766704aec77298438febe113ef
haurujenkins <loic.lpr@gmail.com>
2024-10-23 17:02:19 +0200

    not sure its wrking

--- CPP06/ex00/srcs/main.cpp
+++ CPP06/ex00/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lle-pier <lle-pier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 15:41:36 by lle-pier          #+#    #+#             */
-/*   Updated: 2024/10/23 16:02:57 by lle-pier         ###   ########.fr       */
+/*   Updated: 2024/10/23 16:45:40 by lle-pier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,6 @@
 int main(void)
 {
     ScalarConverter::convert("-89");
+    ScalarConverter::convert("b");
+    ScalarConverter::convert("let's go");
 }
