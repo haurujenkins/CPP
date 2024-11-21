026da275d41231dd32a23da52550326ff88cbbf9
haurujenkins <loic.lpr@gmail.com>
2024-11-21 16:40:40 +0100

    main arrange

--- CPP06/ex01/srcs/main.cpp
+++ CPP06/ex01/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lle-pier <lle-pier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 14:59:43 by lle-pier          #+#    #+#             */
-/*   Updated: 2024/11/21 15:19:31 by lle-pier         ###   ########.fr       */
+/*   Updated: 2024/11/21 16:40:09 by lle-pier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,16 @@ int main(void)
     data.s2 = "World";
 
     unsigned long serialized = Serializer::serialize(&data);
+    
     Data *deserialized = Serializer::deserialize(serialized);
 
-    std::cout << "s1: " << deserialized->s1 << std::endl;
-    std::cout << "n: " << deserialized->n << std::endl;
-    std::cout << "s2: " << deserialized->s2 << std::endl;
+    if (deserialized->s1 == data.s1 && deserialized->n == data.n && deserialized->s2 == data.s2)
+        std::cout << "Data successfully serialized and deserialized" << std::endl;
+    else
+        std::cout << "Data not successfully serialized and deserialized" << std::endl;
+
+    std::cout << "\nDeserialized data:" << std::endl;
+    std::cout << "s1: " << deserialized->s1;
+    std::cout << " | n: " << deserialized->n;
+    std::cout << " | s2: " << deserialized->s2 << std::endl;
 }
